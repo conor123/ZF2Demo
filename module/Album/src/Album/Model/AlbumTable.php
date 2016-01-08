077bf0e5eaa70758c1030672604e7a51162d1c73
Conor Ryan <conor.ryan@safechargecardservices.com>
2016-01-08 16:07:11 +0000

    Adds paginator to albums list

--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -1,25 +1,48 @@
 <?php
 namespace Album\Model;
 
- use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
 
- class AlbumTable
- {
-     protected $tableGateway;
+class AlbumTable
+{
+    protected $tableGateway;
 
-     public function __construct(TableGateway $tableGateway)
-     {
-         $this->tableGateway = $tableGateway;
-     }
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
 
-     public function fetchAll()
-     {
-         $resultSet = $this->tableGateway->select();
-         return $resultSet;
-     }
+    public function fetchAll($paginated=false)
+    {
+        if ($paginated) {
+             // create a new Select object for the table album
+             $select = new Select('album');
+             // create a new result set based on the Album entity
+             $resultSetPrototype = new ResultSet();
+             $resultSetPrototype->setArrayObjectPrototype(new Album());
+             // create a new pagination adapter object
+             $paginatorAdapter = new DbSelect(
+                 // our configured select object
+                 $select,
+                 // the adapter to run it against
+                 $this->tableGateway->getAdapter(),
+                 // the result set to hydrate
+                 $resultSetPrototype
+             );
+             $paginator = new Paginator($paginatorAdapter);
+             return $paginator;
+        }
 
-     public function getAlbum($id)
-     {
+        $resultSet = $this->tableGateway->select();
+        return $resultSet;
+    }
+
+    public function getAlbum($id)
+    {
          $id  = (int) $id;
          $rowset = $this->tableGateway->select(array('id' => $id));
          $row = $rowset->current();
@@ -27,10 +50,10 @@ namespace Album\Model;
              throw new \Exception("Could not find row $id");
          }
          return $row;
-     }
+    }
 
-     public function saveAlbum(Album $album)
-     {
+    public function saveAlbum(Album $album)
+    {
          $data = array(
              'artist' => $album->artist,
              'title'  => $album->title,
@@ -46,10 +69,10 @@ namespace Album\Model;
                  throw new \Exception('Album id does not exist');
              }
          }
-     }
+    }
 
-     public function deleteAlbum($id)
-     {
-         $this->tableGateway->delete(array('id' => (int) $id));
-     }
- }
+    public function deleteAlbum($id)
+    {
+        $this->tableGateway->delete(array('id' => (int) $id));
+    }
+}
